76084da3c3ca120dd2031fbfcf84d711aa411a55
a-gallina <andreagallina.ga@gmail.com>
2026-09-17 12:51:11 +0200

    Correzioni agli header

--- src/matrix.h
+++ src/matrix.h
@@ -3,7 +3,7 @@
 
 void scoring(int n, int m, char *seq1, char *seq2, int matrix[n+1][m+1], int direction[n+1][m+1]);
 #define gap_penalty -1
-#define mismatsch -1
+#define mismatch -1
 #define match 1
 
 #endif
--- src/traceback.h
+++ src/traceback.h
@@ -1,9 +1,9 @@
 #ifndef TRACEBACK_H
 #define TRACEBACK_H
 
-void traceback(int n, int m, char *seq1, char *seq2, int direction[n+1][m+1], int inv_result[n+1][m+1]);
+void traceback(int n, int m, char *seq1, char *seq2, int direction[n+1][m+1], int inv_result[n][m]);
 #define gap_penalty -1
-#define mismatsch -1
+#define mismatch -1
 #define match 1
 
 #endif
